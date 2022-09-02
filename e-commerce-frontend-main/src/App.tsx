5f7d7926e81b563c383e370534f29e97a07aaad5
Tommy Hai <tom.h07@gmail.com>
2022-09-02 16:37:25 -0400

    Revert "Update with the button"
    This reverts commit 54ac6111c65cd293d819795827d2645f77ac271a.
    
    Reverting past 2 commits

--- e-commerce-frontend-main/src/App.tsx
+++ e-commerce-frontend-main/src/App.tsx
@@ -1,15 +1,11 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
-import './index.css';
 import { CartContext } from './context/cart.context';
 import { SiteThemeContext } from './context/sitetheme.context';
 import ProductItem from './models/Product';
 import SiteTheme from './models/SiteTheme';
 import { AppRoutes } from './router/AppRoutes';
-import{ThemeProvider} from "styled-components";
-import {GlobalStyles} from "./components/GlobalStyles";
-import {lightTheme, darkTheme} from "./components/Themes";
 
 function App() {
   const [cart, setCart] = useState<ProductItem[]>([]);
@@ -17,16 +13,7 @@ function App() {
 
   const [sitetheme, setSitetheme] = useState<SiteTheme>(new SiteTheme(''));
   const themevalue = { sitetheme, setSitetheme};
-
-  const [theme, setTheme] = useState('light');
-  const themeToggler = () => {
-    theme === 'light' ? setTheme('dark') : setTheme('light')
-}
-
   return (
-    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
-      <GlobalStyles/>
-      <button onClick={themeToggler}>Switch Theme</button>
     <CartContext.Provider value={value}>
     <SiteThemeContext.Provider value={themevalue}>
       <Router>
@@ -34,7 +21,6 @@ function App() {
       </Router>
     </SiteThemeContext.Provider>
     </CartContext.Provider>
-    </ThemeProvider>
   );
 }
 
