54ac6111c65cd293d819795827d2645f77ac271a
kndoan <katelyn592@revature.net>
2022-09-02 14:50:59 -0400

    Update with the button

---
+++ e-commerce-frontend-main/src/components/Themes.js
@@ -0,0 +1,12 @@
+export const lightTheme = {
+    body: '#FFFFF',
+    text: '#72A4C2',
+    toggleBorder: '#FCB414',
+    background: '#363537',
+}
+export const darkTheme = {
+    body: '#474C55',
+    text: '#F26925',
+    toggleBorder: '#F26925',
+    background: '#F26925',
+}
