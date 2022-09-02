f829655bbfb521f1e083f228c7b52bfcedadadd2
Tommy Hai <tom.h07@gmail.com>
2022-09-02 16:38:10 -0400

    Revert "sitetheme context set up. cart example not included"
    This reverts commit 4545f8db8397c65ed16ae4191491f3025c34f670.
    
    The second half of reverting the past 2 commits

--- e-commerce-frontend-main/src/App.tsx
+++ e-commerce-frontend-main/src/App.tsx
@@ -2,24 +2,18 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import { CartContext } from './context/cart.context';
-import { SiteThemeContext } from './context/sitetheme.context';
 import ProductItem from './models/Product';
-import SiteTheme from './models/SiteTheme';
 import { AppRoutes } from './router/AppRoutes';
 
 function App() {
   const [cart, setCart] = useState<ProductItem[]>([]);
   const value = { cart, setCart };
 
-  const [sitetheme, setSitetheme] = useState<SiteTheme>(new SiteTheme(''));
-  const themevalue = { sitetheme, setSitetheme};
   return (
     <CartContext.Provider value={value}>
-    <SiteThemeContext.Provider value={themevalue}>
       <Router>
         <AppRoutes></AppRoutes>
       </Router>
-    </SiteThemeContext.Provider>
     </CartContext.Provider>
   );
 }
