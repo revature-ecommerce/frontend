869660c5a371af7c9e379168a94f3851bbce8cab
afrumkin <108278558+afrumkin@users.noreply.github.com>
2022-09-08 15:25:08 -0400

    Add files via upload

---
+++ src/router/AppRoutes.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route, Routes } from "react-router-dom";
+import { Cart } from '../components/cart/Cart';
+import Checkout from '../components/checkout/Checkout';
+import { DisplayProducts } from "../components/display-products/DisplayProducts";
+import Login from '../components/login/Login';
+import Register from '../components/register/Register';
+import { ViewUser } from '../components/profile-view/ProfileView';
+
+export const AppRoutes: React.FC<unknown> = () => (
+  <Routes>
+    <Route path="/" element={<DisplayProducts />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/register" element={<Register />} />
+    <Route path="/checkout" element={<Checkout />} />
+    <Route path="/cart" element={<Cart />} />
+    <Route path="/profile" element={<ViewUser />} />
+  </Routes>
+)
