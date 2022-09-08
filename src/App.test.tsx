d26230ecaca756a73bad761115c5ab74bcfec2ed
afrumkin <108278558+afrumkin@users.noreply.github.com>
2022-09-08 15:26:04 -0400

    Delete src directory

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('render app', () => {
-  render(<App />);
-  expect(screen).toBeTruthy;
-});
