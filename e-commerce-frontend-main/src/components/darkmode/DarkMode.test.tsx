fdc5572ce7c2bad7268761c3a39a32a7de33f340
kndoan <katelyn592@revature.net>
2022-09-12 15:26:14 -0400

    fixed darkmode test

--- e-commerce-frontend-main/src/components/darkmode/DarkMode.test.tsx
+++ e-commerce-frontend-main/src/components/darkmode/DarkMode.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen, fireEvent, cleanup } from "@testing-library/react";
 import DarkMode from './DarkMode';
 import { BrowserRouter } from "react-router-dom";
-import Navbar from '../navbar/Narbar';
+import Navbar from '../navbar/Narbar'; 
 
 afterEach(cleanup);
 
@@ -26,19 +26,6 @@ test("checks toggle button", () => {
   expect(fireEvent.click(inputElement)).toBeTruthy();
 });
 
-/**
- * :root {
-  --font-color: rgb(253,181,21);
-  --background-color: rgb(185,185,186);
-  --link-color: rgb(115,165,194);
-}
-  
-[data-theme="dark"] {
-  --font-color: rgb(255, 255, 255);
-  --background-color: rgb(72, 76, 86);
-  --link-color: rgb(242,105,38);
-}
- */
 test("Check Logo",()=>{
   render(
   <BrowserRouter>
